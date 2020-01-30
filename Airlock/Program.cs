5c83a8a09556b13de39c7b1f29d7bb744d115081
RexTheCapt <rexthecapt@gmail.com>
2020-01-30 21:06:37 +0100

    update

---
+++ Airlock/Program.cs
@@ -0,0 +1,372 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // This file contains your actual script.
+        //
+        // You can either keep all your code here, or you can create separate
+        // code files to make your program easier to navigate while coding.
+        //
+        // In order to add a new utility class, right-click on your project, 
+        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
+        // category under 'Visual C# Items' on the left hand side, and select
+        // 'Utility Class' in the main area. Name it in the box below, and
+        // press OK. This utility class will be merged in with your code when
+        // deploying your final script.
+        //
+        // You can also simply create a new utility class manually, you don't
+        // have to use the template if you don't want to. Just do so the first
+        // time to see what a utility class looks like.
+        // 
+        // Go to:
+        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
+        //
+        // to learn more about ingame scripts.
+        
+        private List<IMyAirVent> airVents = new List<IMyAirVent>();
+        private List<IMyDoor> doors = new List<IMyDoor>();
+        private List<IMyInteriorLight> lights = new List<IMyInteriorLight>();
+
+        public Program()
+        {
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+            // 
+            // It's recommended to set Runtime.UpdateFrequency 
+            // here, which will allow your script to run itself without a 
+            // timer block.
+
+            GetAllBlocks();
+
+            foreach (IMyAirVent vent in airVents)
+            {
+                VentData vd = new VentData(vent);
+
+                if (vd.IsAirlock)
+                {
+                    vd.Reset();
+                    Echo($"Reset {vd.Name}");
+                }
+            }
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked,
+            // or the script updates itself. The updateSource argument
+            // describes where the update came from. Be aware that the
+            // updateSource is a  bitfield  and might contain more than 
+            // one update type.
+            // 
+            // The method itself is required, but the arguments above
+            // can be removed if not needed.
+
+            GetAllBlocks();
+
+            // Drain or fill airlock
+            foreach (IMyAirVent airVent in airVents)
+            {
+                VentData vd = new VentData(airVent);
+
+                Echo(lights.Count.ToString());
+
+                foreach (IMyInteriorLight light in lights)
+                {
+
+                }
+
+                if (vd.IsAirlock)
+                {
+                    foreach (IMyDoor door in doors)
+                    {
+                        if (door.CustomName.Contains($"[Airlock-{vd.Group}"))
+                        {
+                            switch (vd.Direction)
+                            {
+                                case VentData.Direct.Inn:
+                                    airVent.Depressurize = false;
+
+                                    if (door.CustomName.Contains("Inn"))
+                                    {
+                                        if ((!airVent.CanPressurize || airVent.GetOxygenLevel() < 0.95))
+                                        {
+                                            if(door.OpenRatio != 0)
+                                            {
+                                                door.Enabled = true;
+                                                door.CloseDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                        }
+                                        else if (airVent.GetOxygenLevel() > 0.95)
+                                            if (door.OpenRatio != 1)
+                                            {
+                                                door.Enabled = true;
+                                                door.OpenDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                    }
+                                    else if (door.CustomName.Contains("Out"))
+                                    {
+                                        if ((!airVent.CanPressurize || airVent.GetOxygenLevel() < 0.95))
+                                        {
+                                            if (door.OpenRatio != 0)
+                                            {
+                                                door.Enabled = true;
+                                                door.CloseDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                        }
+                                        else if (airVent.GetOxygenLevel() > 0.95)
+                                            if (door.OpenRatio != 0)
+                                            {
+                                                door.Enabled = true;
+                                                door.OpenDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                    }
+
+                                    if (airVent.GetOxygenLevel() > 0.05)
+                                        SetLights(vd.Group, AirlockStatus.Inn);
+                                    else if (!airVent.CanPressurize || airVent.GetOxygenLevel() < 0.05)
+                                        SetLights(vd.Group, AirlockStatus.Working);
+                                    break;
+                                case VentData.Direct.Out:
+                                    if (door.CustomName.Contains("Inn"))
+                                    {
+                                        if (door.OpenRatio != 0)
+                                        {
+                                            door.Enabled = true;
+                                            door.CloseDoor();
+                                        }
+                                        else
+                                        {
+                                            door.Enabled = false;
+                                            airVent.Depressurize = true;
+                                        }
+                                    }
+                                    else if (door.CustomName.Contains("Out"))
+                                    {
+                                        if (airVent.GetOxygenLevel() > 0.05)
+                                        {
+                                            if (door.OpenRatio != 0)
+                                            {
+                                                door.Enabled = true;
+                                                door.CloseDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                        }
+                                        else if (!airVent.CanPressurize || airVent.GetOxygenLevel() == 0)
+                                        {
+                                            if (door.OpenRatio != 1)
+                                            {
+                                                door.Enabled = true;
+                                                door.OpenDoor();
+                                            }
+                                            else
+                                            {
+                                                door.Enabled = false;
+                                            }
+                                        }
+                                    }
+
+                                    if(airVent.GetOxygenLevel() > 0)
+                                        SetLights(vd.Group, AirlockStatus.Working);
+                                    else if (airVent.GetOxygenLevel() == 0 || !airVent.CanPressurize)
+                                        SetLights(vd.Group, AirlockStatus.Out);
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsAllDoorsClosed(string group)
+        {
+            foreach (IMyDoor door in doors)
+            {
+                if (door.CustomName.Contains($"[Airlock-{group}"))
+                {
+                    if (door.OpenRatio != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private void SetLights(string group, AirlockStatus status)
+        {
+            List<IMyInteriorLight> innLights = new List<IMyInteriorLight>();
+            List<IMyInteriorLight> outLights = new List<IMyInteriorLight>();
+            List<IMyInteriorLight> allLights = new List<IMyInteriorLight>();
+
+            foreach (IMyInteriorLight light in lights)
+            {
+                if (light.CustomName.Contains($"[Airlock-{group}"))
+                {
+                    if(light.CustomName.Contains("Inn"))
+                        innLights.Add(light);
+                    else if(light.CustomName.Contains("Out"))
+                        outLights.Add(light);
+                }
+            }
+
+            allLights.AddRange(innLights);
+            allLights.AddRange(outLights);
+
+            switch (status)
+            {
+                case AirlockStatus.Inn:
+                    foreach (IMyInteriorLight light in innLights)
+                    {
+                        light.Color = Color.Green;
+                        light.BlinkIntervalSeconds = 0;
+                        light.BlinkLength = 0;
+                    }
+
+                    foreach (IMyInteriorLight light in outLights)
+                    {
+                        light.Color = Color.DarkRed;
+                        light.BlinkIntervalSeconds = 1;
+                        light.BlinkLength = 25;
+                    }
+                    break;
+                case AirlockStatus.Out:
+                    foreach (IMyInteriorLight light in outLights)
+                    {
+                        light.Color = Color.Green;
+                        light.BlinkIntervalSeconds = 0;
+                        light.BlinkLength = 0;
+                    }
+
+                    foreach (IMyInteriorLight light in innLights)
+                    {
+                        light.Color = Color.DarkRed;
+                        light.BlinkIntervalSeconds = 1;
+                        light.BlinkLength = 25;
+                    }
+                    break;
+                case AirlockStatus.Working:
+                    foreach (IMyInteriorLight light in allLights)
+                    {
+                        light.Color = Color.Orange;
+                        light.BlinkIntervalSeconds = 0;
+                        light.BlinkLength = 0;
+                    }
+                    break;
+            }
+        }
+
+        private enum AirlockStatus
+        {
+            Inn,
+            Out,
+            Working,
+            Error
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        private void GetAllBlocks()
+        {
+            GridTerminalSystem.GetBlocksOfType(airVents);
+            GridTerminalSystem.GetBlocksOfType(doors);
+            GridTerminalSystem.GetBlocksOfType(lights);
+        }
+    }
+
+    internal class VentData
+    {
+        public string Name => AirVent.CustomName;
+        public bool IsAirlock => Name.Contains("[Airlock");
+        public string Group => Name.Split('-')[1].Replace("]", "");
+
+        public IMyAirVent AirVent;
+
+        public Direct Direction;
+
+        public VentData(IMyAirVent vent)
+        {
+            AirVent = vent;
+
+            string lines = vent.CustomData;
+
+            if (!string.IsNullOrEmpty(vent.CustomData))
+            {
+                Direction = lines.Split(':')[1] == "Inn" ? Direct.Inn : Direct.Out;
+            }
+            else
+            {
+                Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            AirVent.CustomData = $"dir:Inn";
+        }
+
+        public void Save()
+        {
+            AirVent.CustomData = $"dir:{(Direction == Direct.Inn ? "Inn" : "Out")}";
+        }
+
+        public enum Direct
+        {
+            Inn,
+            Out
+        }
+    }
+}
---
+++ CargoStatus/Program.cs
@@ -0,0 +1,179 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // This file contains your actual script.
+        //
+        // You can either keep all your code here, or you can create separate
+        // code files to make your program easier to navigate while coding.
+        //
+        // In order to add a new utility class, right-click on your project, 
+        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
+        // category under 'Visual C# Items' on the left hand side, and select
+        // 'Utility Class' in the main area. Name it in the box below, and
+        // press OK. This utility class will be merged in with your code when
+        // deploying your final script.
+        //
+        // You can also simply create a new utility class manually, you don't
+        // have to use the template if you don't want to. Just do so the first
+        // time to see what a utility class looks like.
+        // 
+        // Go to:
+        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
+        //
+        // to learn more about ingame scripts.
+
+        public Program()
+        {
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+            // 
+            // It's recommended to set Runtime.UpdateFrequency 
+            // here, which will allow your script to run itself without a 
+            // timer block.
+
+            Runtime.UpdateFrequency = UpdateFrequency.Once;
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked,
+            // or the script updates itself. The updateSource argument
+            // describes where the update came from. Be aware that the
+            // updateSource is a  bitfield  and might contain more than 
+            // one update type.
+            // 
+            // The method itself is required, but the arguments above
+            // can be removed if not needed.
+
+            List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
+            GridTerminalSystem.GetBlocksOfType(blocks: cargoContainers);
+
+            List<MyInventoryItem> inventoryItems = new List<MyInventoryItem>();
+
+            foreach (IMyCargoContainer container in cargoContainers)
+            {
+                IMyInventory myInventory = container.GetInventory();
+                List<MyInventoryItem> items = new List<MyInventoryItem>();
+                myInventory.GetItems(items: items);
+
+                foreach (MyInventoryItem item in items)
+                {
+                    inventoryItems.Add(item: item);
+                }
+            }
+
+            List<ItemCount> itemCounts = new List<ItemCount>();
+
+            foreach (MyInventoryItem item in inventoryItems)
+            {
+                if (itemCounts.All(x => x.Type != item.Type))
+                {
+                    ItemCount itemCount = new ItemCount(item.Type);
+                    itemCounts.Add(itemCount);
+
+                    itemCount.Amount += item.Amount;
+                }
+                else
+                {
+                    ItemCount itemCount = itemCounts.FirstOrDefault(x => x.Type == item.Type);
+
+                    if (itemCount != null)
+                    {
+                        itemCount.Amount += item.Amount;
+                    }
+                }
+            }
+
+            foreach (ItemCount count in itemCounts.OrderBy(x => x.Type).ToList())
+            {
+                Echo("1");
+                Echo($"{count} : {count.Amount}");
+            }
+        }
+
+        class ItemCount
+        {
+            public readonly MyItemType Type;
+
+            public ItemCount(MyItemType type)
+            {
+                Type = type;
+            }
+
+            public MyFixedPoint Amount = 0;
+
+            public override string ToString()
+            {
+                string output = "";
+
+                switch (Type.TypeId)
+                {
+                    case "MyObjectBuilder_PhysicalGunObject":
+                        output += "[Tool]";
+                        break;
+                    case "MyObjectBuilder_Component":
+                        output+="[Comp]";
+                        break;
+                    case "MyObjectBuilder_Ore":
+                        output += "[Ore]";
+                        break;
+                    case "MyObjectBuilder_Ingot":
+                        output += "[Ingot]";
+                        break;
+                    case "MyObjectBuilder_Datapad":
+                        output += "[Pad]";
+                        break;
+                    case "MyObjectBuilder_AmmoMagazine":
+                        output += "[Ammo]";
+                        break;
+                    case "MyObjectBuilder_PhysicalObject":
+                        output += "[Object]";
+                        break;
+                    case "MyObjectBuilder_ConsumableItem":
+                        output += "[Consume]";
+                        break;
+                    default:
+                        output += $"[{Type.TypeId}]";
+                        break;
+                }
+
+                return $"{output} {Type.SubtypeId}";
+            }
+        }
+    }
+}
